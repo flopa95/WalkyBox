9e6e8ea8aac4e99524e4ee8c939618061534d0ee
flopa95 <fparzhuber.2013@business.smu.edu.sg>
2017-03-13 23:34:14 +0800

    lol

--- main.py
+++ main.py
@@ -1,60 +1,42 @@
 from tkinter import *
 import time
 
-x = 10
-y = 10
+x = 20
+y = 20
+
+snakelength = 4
+boxes = []
+
 
 root = Tk()
 canvas = Canvas(root, width = 500, height = 500)
-box = canvas.create_rectangle(10, 10, 30, 30, fill="red", outline="black")
 
+for i in range (1,snakelength):
+    boxes.append(canvas.create_rectangle(30*i, 10, (30*(i+1)), 40, fill="red", outline="black"))
 canvas.pack()
-
-def draw_stuff():
-    global x, y
-    x = x
-    y = y
-    #box = canvas.create_rectangle(10, 10, 30, 30, fill="red", outline="black")
-    #canvas.delete(box)
-    canvas.move(box,x,0)
-    root.update()
-    #root.after(20,draw_stuff)
-    #print("x")
-
 
 
 def go_right(event):
-    coords = canvas.coords(box)
-    if coords[2] < 500:
-        canvas.move(box, x, 0)
-        root.update()
+    global boxes, snakelength
+    boxcoords = []
+    for i in range (0,len(boxes)):
+        boxcoords.append(canvas.coords(boxes[i]))
 
-def go_left(event):
-    coords = canvas.coords(box)
-    if coords[0] > 0:
-        canvas.move(box, -x, 0)
-        root.update()
+    if boxcoords[0][2] < 490:
+        for i in reversed(range(0,len(boxes)-1)):
+            canvas.coords(boxes[i+1],boxcoords[i])
+            canvas.move(boxes[len(boxes)-1], x, 0)
+            root.update()
 
-def go_up(event):
-    coords = canvas.coords(box)
-    if coords[1] > 0:
-        canvas.move(box, 0, -y)
-        root.update()
-
-def go_down(event):
-    coords = canvas.coords(box)
-    if coords[3] < 500:
-        canvas.move(box, 0, y)
-        root.update()
 
 
 
 root.bind("<Right>", go_right)
-root.bind("<Left>", go_left)
-root.bind("<Up>", go_up)
-root.bind("<Down>", go_down)
+#root.bind("<Left>", go_left)
+#root.bind("<Up>", go_up)
+#root.bind("<Down>", go_down)
 
-root.after(0,draw_stuff)
+
 root.mainloop()
 
 
